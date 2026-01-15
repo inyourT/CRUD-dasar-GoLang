24d5dcbedfff8876c5edea0026ad74978c282cda
inyourT <gagunatulis@gmail.com>
2026-01-15 14:34:00 +0700

    feat:model & controller

--- controller/imp/mahasiswa_controller_imp.go
+++ controller/imp/mahasiswa_controller_imp.go
@@ -2,9 +2,14 @@ package imp
 
 import (
 	"crud-dasar/controller"
+	"crud-dasar/helper"
+	"crud-dasar/model/entity/web"
+	"crud-dasar/model/entity/web/mahasiswa"
 	"crud-dasar/service"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type MahasiswaControllerImp struct {
@@ -12,31 +17,80 @@ type MahasiswaControllerImp struct {
 }
 
 func NewMahasiswaController(mahasiswaService service.MahasiswaService) controller.MahasiswaController {
-	return &MahasiswaControllerImp{}
+	return &MahasiswaControllerImp{
+		MahasiswaService: mahasiswaService,
+	}
 }
 
 // Craete implements [controller.MahasiswaController].
 func (m *MahasiswaControllerImp) Craete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	panic("unimplemented")
+	mahasiswaCreateRequest := mahasiswa.MahasiswaCreateRequest{}
+	helper.ReadFromRequestBody(r, &mahasiswaCreateRequest)
+
+	mahasiswaResponse := m.MahasiswaService.Create(r.Context(), mahasiswaCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   http.StatusCreated,
+		Status: "CREATED",
+		Data:   mahasiswaResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 // Delete implements [controller.MahasiswaController].
 func (m *MahasiswaControllerImp) Delete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	panic("unimplemented")
+	mahasiswaID := p.ByName("mahasiswa")
+	id, err := strconv.Atoi(mahasiswaID)
+	helper.PanicIfError(err)
+
+	m.MahasiswaService.Delete(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 // FindAll implements [controller.MahasiswaController].
 func (m *MahasiswaControllerImp) FindAll(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	panic("unimplemented")
+	mahasiswaRespone := m.MahasiswaService.FindAll(r.Context())
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   mahasiswaRespone,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 // FindByID implements [controller.MahasiswaController].
 func (m *MahasiswaControllerImp) FindByID(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	panic("unimplemented")
+	mahasiswaID := p.ByName("mahasiswa")
+	id, err := strconv.Atoi(mahasiswaID)
+	helper.PanicIfError(err)
+
+	mahasiswaRespone := m.MahasiswaService.FindByID(r.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   mahasiswaRespone,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
 }
 
 // Update implements [controller.MahasiswaController].
 func (m *MahasiswaControllerImp) Update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	panic("unimplemented")
-}
+	mahasiswaUpdateRequest := mahasiswa.MahasiswaUpdateRequest{}
+	helper.ReadFromRequestBody(r, &mahasiswaUpdateRequest)
 
+	mahasiswaResponse := m.MahasiswaService.Update(r.Context(), mahasiswaUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "UPDATED",
+		Data:   mahasiswaResponse,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
--- model/entity/web/mahasiswa/mahasiswa_create_request.go
+++ model/entity/web/mahasiswa/mahasiswa_create_request.go
@@ -2,7 +2,7 @@ package mahasiswa
 
 type MahasiswaCreateRequest struct {
 	Nama         string `validate:"required, max=100, min=3" json:"name"`
-	Nim          int `validate:"required, max=100, min=3" json:"nim"`
+	Nim          int    `validate:"required, max=100, min=3" json:"nim"`
 	Alamat       string `validate:"required, max=100, min=3" json:"alamat"`
 	JenisKelamin string `validate:"required, max=100, min=3" json:"jenis_kelamin"`
 }

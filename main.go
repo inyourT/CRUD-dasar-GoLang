0cc56e225fac36a6d2b823d6c5fe631cfe6356be
inyourT <gagunatulis@gmail.com>
2026-01-15 14:34:36 +0700

    feat:routing/link

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"crud-dasar/app"
+	controller "crud-dasar/controller/imp"
+	"crud-dasar/helper"
+	repository "crud-dasar/repository/impl"
+	service "crud-dasar/service/impl"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 
 	db := app.NewDB()
+	validate := validator.New()
+
+	mahasiswaRepository := repository.NewMahasiswaRepository()
+	mahasiswaService := service.NewMahasiswaService(mahasiswaRepository, db, validate)
+	mahasiswaController := controller.NewMahasiswaController(mahasiswaService)
+
+	app.NewRouter(mahasiswaController)
 
 	server := http.Server{
 		Addr: "localhost:3000",
 	}
 
 	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	helper.PanicIfError(err)
 }

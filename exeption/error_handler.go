589a24c267a17a90e47c1492207ecb7025434807
inyourT <gagunatulis@gmail.com>
2026-01-15 14:29:58 +0700

    feat: Error halper

---
+++ exeption/error_handler.go
@@ -0,0 +1,49 @@
+package exeption
+
+import (
+	"crud-dasar/helper"
+	"crud-dasar/model/entity/web"
+	"net/http"
+)
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
+	if notFoundError(w, r, err) {
+		return
+	}
+
+	internalServerError(w, r, err)
+}
+
+func notFoundError(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(NotFoundError)
+
+	if ok {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusNotFound,
+			Status: "NOT FOUND ERROR",
+			Data:   exception.Error,
+		}
+
+		helper.WriteToResponseBody(w, webResponse)
+
+		return true
+	} else {
+		return false
+	}
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request, err interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+
+	webResponse := web.WebResponse{
+		Code:   http.StatusInternalServerError,
+		Status: "Internal Server Error",
+		Data:   err,
+	}
+
+	helper.WriteToResponseBody(w, webResponse)
+}
